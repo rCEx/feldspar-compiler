b05134b33da05fb3b67096bd901306839dc526ba
ivar <ivar@8560w.(none)>
2013-05-06 13:48:28 +0200

    Removed maybe call to af in Binding to avoid re-declaration of variable.

--- lib/Feldspar/Compiler/Imperative/FromCore/Loop.hs
+++ lib/Feldspar/Compiler/Imperative/FromCore/Loop.hs
@@ -100,18 +100,18 @@ instance ( Compile dom dom
                typ3 = compileTypeRep ta3 sa3
           in --maybe Skip (\f -> f [bound]) af .>>
             case typ3 of PIRE.TPointer _ ->  compileProgWithName out outc af init m .>>
-                                              Alloc typ3 $ \temp tempc tempAf -> let (Assign _ xs _) = snd out (undefined)
+                                              Alloc typ3 $ \temp tempc tempAf -> let (Assign _ xs _) = snd out undefined
                                                                                      (Index n _)     = fst out
                                                                                  in
                                               --loc temp (Index n xs) .>>
                                               for (Num 0) bound $ \e -> 
-                                               compileLets bs1 
+                                               compileLets bs1
                                                            (compileProgWithName (var temp, locArray temp e) (Just tempc) (Just tempAf) ixf)
                                                            (M.insert st (fst out) $ M.insert ix e m)
                                                .>> snd out (var temp)
 
 
-                         _               -> compileProgWithName out Nothing Nothing init m .>>
+                         _               -> compileProgWithName out outc af init m .>>
                                              Decl typ3 $ \temp -> let (Assign _ xs _) = snd out (undefined)
                                                                       (Index n _)     = fst out
                                                                   in 

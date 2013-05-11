1c47fb1c7b04f66dbe18ffdee5797f336aed1310
ivar <ivar@ghostly.(none)>
2013-05-11 20:46:43 +0200

    More on fold

--- test.hs
+++ test.hs
@@ -5,10 +5,12 @@ import Feldspar.Vector
 --import Feldspar.Repa
 import ParScan
 
-import FFT.Pull
+--import FFT.Pull
 
 import Feldspar.Compiler
 import Feldspar.Compiler.Imperative.FromCore
+
+import Debug.Trace
 
 
 test :: Data [Index]
@@ -27,8 +29,8 @@ vecMul :: Vector1 Index -> Vector1 Index -> Vector1 Index
 vecMul = zipWith (*)
 
 
-fftInt :: Data Index -> Vector1 Index -> Vector1 Index
-fftInt n xs = fftCore n (+) xs
+--fftInt :: Data Index -> Vector1 Index -> Vector1 Index
+--fftInt n xs = fftCore n (+) xs
 
 
 
@@ -42,12 +44,13 @@ seqFold f init xs = forLoop (length xs) init $ \i acc -> f acc (xs ! i)
 
 
 parFold :: Syntax a => (a -> a -> a) -> Vector a -> Vector a
-parFold f xs = forLoop (log2 (length xs)) xs $ \i' acc -> let i = max 1 (i'*2) in indexed (length acc) $ \j -> condition 
-                                                                                             (j `mod` (2*i) == 0)
-                                                                                             (f (acc ! j) 
-                                                                                                (acc ! (j+i)
-                                                                                                ))
-                                                                                             (acc ! j)
+parFold f xs = forLoop ((log2 $ length xs)) xs $ \i' acc -> indexed (length acc) $ \j -> condition 
+                                                                                          (j `mod` (2^i') == 0)
+                                                                                          (f (acc ! j) 
+                                                                                             (acc ! (j+(2^(i'-1)))))
+                                                                                          (acc ! j)
+
+
 foldTest :: Vector1 Index -> Vector1 Index
 foldTest xs = parFold (+) xs
 

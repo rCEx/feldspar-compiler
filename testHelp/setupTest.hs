5d9d46cec72f9afb7df9a94b49b21b3612c862fe
ivar <ivar@8560w.(none)>
2013-05-13 15:43:21 +0200

    Adding test things.

---
+++ testHelp/setupTest.hs
@@ -0,0 +1,16 @@
+
+
+main :: IO ()
+main = undefined
+
+-- 1 input vector - 1 output vector
+in1VecOut1Sca :: String
+in1VecOut1Sca = undefined 
+
+
+-- 2 input vectors - 1 output scalar
+in2VecOut1Sca :: String
+in2VecOut1Sca = undefined 
+
+
+

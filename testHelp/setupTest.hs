15861dc020b6a7e73fad64e67e5b14c676c726b2
ivar <ivar@8560w.(none)>
2013-05-13 16:00:13 +0200

    Added string for an output routine

--- testHelp/setupTest.hs
+++ testHelp/setupTest.hs
@@ -13,4 +13,11 @@ in2VecOut1Sca :: String
 in2VecOut1Sca = undefined 
 
 
-
+timeMeasure :: String
+timeMeasure = "void outputMeasure(char *to, time_t time) {\n" ++
+              "  FILE *fp = fopen(to, \"a\");\n"              ++
+              "  if(fp != NULL) {\n"                          ++
+              "    fprintf(fp, \"%li\\n\", time);\n"          ++
+              "  }\n"                                         ++
+              "  fclose(to);\n"                               ++
+              "}\n"

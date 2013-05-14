e6b2dfe6a18342725725a8f6e13c08f0b5544a1e
ivar <ivar@8560w.(none)>
2013-05-14 12:23:50 +0200

    the test environment now prints both clock cycles and seconds elapsed

--- testHelp/setupTest.hs
+++ testHelp/setupTest.hs
@@ -1,28 +1,29 @@
 import System.Cmd
 
 main :: IO ()
-main = sequence_ $ map putStrLn $ runAll "./a.out " 15
+main = do system cc
+          sequence_ $ map system $ runAll "./a.out " 22
 
+dotProd = appendFile "main.c" $ timeMeasure ++ main2VecSca "timersDotProd.log"
 
 runAll name n = map ((++) name . show) (sizes n)
 
-cc = "gcc -std=c99 -I C_new/ -I /usr/local/cuda/include/ -lOpenCL C_new/feldspar_c99.c -lm main.c"
-
+cc = "gcc -std=c99 -I ../C_new/ -I /usr/local/cuda/include/ -lOpenCL ../C_new/feldspar_c99.c -lm main.c"
 sizes :: Integer -> [Integer]
 sizes n = [2^i | i <- [0..n]]
 
 timeMeasure :: String
-timeMeasure = "void outputMeasure(char *to, time_t time) {\n" ++
+timeMeasure = "void outputMeasure(char *to, clock_t time, int size) {\n" ++
               "  FILE *fp = fopen(to, \"a\");\n"              ++
               "  if(fp != NULL) {\n"                          ++
-              "    fprintf(fp, \"%li\\n\", time);\n"          ++
+              "    fprintf(fp, \"%li %f %i\\n\", time, ((double)time)/CLOCKS_PER_SEC, size);\n" ++
               "  }\n"                                         ++
-              "  fclose(to);\n"                               ++
+              "  fclose(fp);\n"                               ++
               "}\n"
 
 main2VecSca :: String -> String
 main2VecSca timerLog = 
-  "int main (int argc, char **argv[]) {\n" ++
+  "int main (int argc, char *argv[]) {\n" ++
   "  const int size = atoi(argv[1]);\n" ++
   "  int *a = (int*) malloc(sizeof(int)*size);\n" ++
   "  int *b = (int*) malloc(sizeof(int)*size);\n" ++
@@ -36,7 +37,8 @@ main2VecSca timerLog =
   "  t = clock();\n" ++
   "  f0(a,size,b,size, &res);\n" ++
   "  t = clock() - t;\n" ++
-  "  outputMeasure(" ++ timerLog ++",t, size);\n" ++
+  "  outputMeasure(" ++ "\"" ++ timerLog ++ "\",t, size);\n" ++
+  "  printf(\"%i : res \\n\", res[0]);\n" ++
   "  return 0;\n" ++
   "}\n"
 

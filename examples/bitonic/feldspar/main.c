f727df5cfd1873faf9f1ec3da2ff88d3af219461
ivar <ivar@8560w.(none)>
2013-05-20 11:03:58 +0200

    updated main

--- examples/bitonic/feldspar/main.c
+++ examples/bitonic/feldspar/main.c
@@ -7,44 +7,40 @@
 #include "bitonic.h"
 #define MAX_SOURCE_SIZE (0x100000)
 
-int main() {
+
+void outputMeasure(char *to, long time, int size) {
+  FILE *fp = fopen(to, "a");
+  if(fp != NULL) {
+    fprintf(fp, "%li %i\n", time, size);
+  }
+  fclose(fp);
+}
+
+
+
+int main (int argc, char *argv[]) {
+  const int size = atoi(argv[1]);
   
- int base = 2;
- int size = 4;
   
- struct array *a = NULL;
- a = initArray(a, sizeof(int), size); 
+  struct array *a = NULL;
+  a = initArray(a, sizeof(int), size); 
 
   int* buf = (int*) a->buffer;
   for(int i = 0; i < size; i++) {
     buf[i] = size-i;
   }
-  //a[0] = 13;
-  //a[1] = 2;
-  //a[2] = 1;
-  //a[3] = 1;
-  //a[4] = 1;
-  //a[5] = 1;
-  //a[6] = 3;
-  //a[7] = 3;
-  //a[8] = 0;
-  //a[9] = 2;
-  //a[10] = 2;
-  //a[11] = 1;
-  //a[12] = 11;
-  //a[13] = 6;
-  //a[14] = 7;
-  //a[15] = 8;
+
   struct array *res = NULL;
   res = initArray(res, sizeof(int), size);
-  f0(base, a, &res);
-  
-  buf = (int*) res->buffer;
-  for(int i = 0; i < size; i++) {
-    printf("%i\n", buf[i]);
-  }
-  
 
+  struct timespec timer1;
+  struct timespec timer2;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &timer1);
+  f0((int)log2(size), a, &res);
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &timer2);
+  long nanos = timer2.tv_nsec - timer1.tv_nsec;
+
+  
 
   return 0;
 }

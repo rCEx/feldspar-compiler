59e83d8408d22197c2974f2b7ea9dee5487774fc
ivar <ivar@8560w.(none)>
2013-06-04 15:31:04 +0200

    modified runTests to start at 1024 and run to 10^24

--- examples/runTests.hs
+++ examples/runTests.hs
@@ -1,9 +1,9 @@
 import System.Cmd
 
 main :: IO ()
-main = sequence_ $ map system $ runAll "./a.out " 23
+main = sequence_ $ map system $ runAll "./a.out " 24
 
 runAll name n = map ((++) name . show) (sizes n)
 
 sizes :: Integer -> [Integer]
-sizes n = [2^i | i <- [1..n]]
+sizes n = [2^i | i <- [10..n]]

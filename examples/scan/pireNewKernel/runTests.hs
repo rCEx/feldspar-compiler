e0ff80eb9714e39e696117a6d64af017a94cb78d
ivar <ivar@8560w.(none)>
2013-06-05 11:48:59 +0200

    Removed pireNewKernel dir

--- examples/scan/pireNewKernel/runTests.hs
+++
@@ -1,9 +0,0 @@
-import System.Cmd
-
-main :: IO ()
-main = sequence_ $ map system $ runAll "./a.out " 24
-
-runAll name n = map ((++) name . show) (sizes n)
-
-sizes :: Integer -> [Integer]
-sizes n = [2^i | i <- [0..n]]

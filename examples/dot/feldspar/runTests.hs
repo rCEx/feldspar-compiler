8a07f8b806307fa0c5b1dd51243e812e625bf91f
ivar <ivar@8560w.(none)>
2013-06-08 13:21:08 +0200

    minor changes

--- examples/dot/feldspar/runTests.hs
+++ examples/dot/feldspar/runTests.hs
@@ -1,9 +1,9 @@
 import System.Cmd
 
 main :: IO ()
-main = sequence_ $ map system $ runAll "./a.out " 24
+main = sequence_ $ map system $ runAll "./a.out " 23
 
 runAll name n = map ((++) name . show) (sizes n)
 
 sizes :: Integer -> [Integer]
-sizes n = [2^i | i <- [0..n]]
+sizes n = [2^i | i <- [10..n]]
